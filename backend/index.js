d10c79b313203996597671a25cc19d066662ead3
guilhermeulbriki <guiulbriki@gmail.com>
2020-01-14 11:52:21 -0300

    39 minutes

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 
 const app = express();
+app.use(express.json);
 
 app.get('/', (request, response) => {
-
+  return response.json({ message: 'Hello Omnistack!'});
 });
 
 app.listen(3333);
