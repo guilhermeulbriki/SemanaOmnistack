407676fffb0a1a44d777f28f29783df572879906
guilhermeulbriki <guiulbriki@gmail.com>
2020-01-14 17:53:17 -0300

    finish first class

--- backend/src/controllers/DevController.js
+++ backend/src/controllers/DevController.js
@@ -1,14 +1,20 @@
 const axios = require('axios');
 const Dev = require('../models/dev');
+const parseStringAsArray = require('../utils/parseStringAsArray');
 
 module.exports = {
+  async index (request, response) {
+    const devs = await Dev.find();
+    return response.json(devs);
+  },
+
   async store (request, response) {
     const { git_user, techs, latitude, longitude } = request.body;
     let dev = await Dev.findOne({ git_user });
     if (!dev) {
       const apiResponse = await axios.get(`https://api.github.com/users/${git_user}`);
       const { name = login, avatar_url, bio } = apiResponse.data;
-      const techArray = techs.split(',').map(tech => tech.trim());
+      const techArray = parseStringAsArray(techs);
       const location = {
         type: 'Point',
         coordinates: [longitude, latitude],
