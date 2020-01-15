f47e5d22a62af078da2468a11224ba481be699ea
guilhermeulbriki <guiulbriki@gmail.com>
2020-01-15 20:08:35 -0300

    resolvendo os problemas do back e 1:22 hr da aula 3

--- web/src/App.js
+++ web/src/App.js
@@ -1,26 +1,150 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import './global.css';
+import './app.css';
+import './Sidebar.css';
+import './Main.css';
 
 function App() {
+  const [git_user, setGitUser] = useState('');
+  const [techs, setTechs] = useState('');
+
+  const [latitude, setLatitude] = useState('');
+  const [longitude, setLongitude] = useState('');
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const { latitude, longitude } = position.coords;
+        setLatitude(latitude);
+        setLongitude(longitude);
+      },
+      (err) => {
+        console.log(err);
+      },
+      {
+        timeout: 30000,
+      }
+    )
+  }, []);
+
+  async function addDev(e) {
+    e.preventDefault();
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="app">
+      <aside>
+        <strong>Cadastrar</strong>
+        <form onSubmit={addDev}>
+          <div className="input_block">
+            <label htmlFor="git_user">Usuário do Github</label>
+            <input
+              name="git-user"
+              id="git_user"
+              required
+              value={git_user}
+              onChange={e => setGitUser(e.target.value)}
+            />
+          </div>
+          <div className="input_block">
+            <label htmlFor="techs">Tecnologias</label>
+            <input
+              name="techs"
+              id="techs"
+              required
+              value={techs}
+              onChange={e => setTechs(e.target.value)}
+            />
+          </div>
+          <div className="input_group">
+            <div className="input_block">
+              <label htmlFor="latitude">Latitude</label>
+              <input
+                type="number"
+                name="latitude"
+                id="latitude"
+                required
+                value={latitude}
+                onChange={e => setLatitude(e.target.value)}
+              />
+            </div>
+            <div className="input_block">
+              <label htmlFor="longitude">Longitude</label>
+              <input
+                type="number"
+                name="longitude"
+                id="longitude"
+                required
+                value={longitude}
+                onChange={e => setLongitude(e.target.value)}
+              />
+            </div>
+          </div>
+          <button type="submit">Salvar</button>
+        </form>
+      </aside>
+      <main>
+        <ul>
+          <li className="devItem">
+            <header>
+              <img src="https://avatars0.githubusercontent.com/u/45516090?s=460&v=4" alt="Guilherme Ulbriki"/>
+              <div className="user_info">
+                <strong>Guilherme Ulbriki</strong>
+                <span>ReactJS, Reacte Native, NodeJS</span>
+              </div>
+            </header>
+            <p>IFFar Tecnico em Informatica - Campus Frederico Westphalen</p>
+            <a href="https://github.com/guilhermeulbriki">Acessar perfil no Github</a>
+          </li>
+          <li className="devItem">
+            <header>
+              <img src="https://avatars0.githubusercontent.com/u/45516090?s=460&v=4" alt="Guilherme Ulbriki"/>
+              <div className="user_info">
+                <strong>Guilherme Ulbriki</strong>
+                <span>ReactJS, Reacte Native, NodeJS</span>
+              </div>
+            </header>
+            <p>IFFar Tecnico em Informatica - Campus Frederico Westphalen</p>
+            <a href="https://github.com/guilhermeulbriki">Acessar perfil no Github</a>
+          </li>
+          <li className="devItem">
+            <header>
+              <img src="https://avatars0.githubusercontent.com/u/45516090?s=460&v=4" alt="Guilherme Ulbriki"/>
+              <div className="user_info">
+                <strong>Guilherme Ulbriki</strong>
+                <span>ReactJS, Reacte Native, NodeJS</span>
+              </div>
+            </header>
+            <p>IFFar Tecnico em Informatica - Campus Frederico Westphalen</p>
+            <a href="https://github.com/guilhermeulbriki">Acessar perfil no Github</a>
+          </li>
+          <li className="devItem">
+            <header>
+              <img src="https://avatars0.githubusercontent.com/u/45516090?s=460&v=4" alt="Guilherme Ulbriki"/>
+              <div className="user_info">
+                <strong>Guilherme Ulbriki</strong>
+                <span>ReactJS, Reacte Native, NodeJS</span>
+              </div>
+            </header>
+            <p>IFFar Tecnico em Informatica - Campus Frederico Westphalen</p>
+            <a href="https://github.com/guilhermeulbriki">Acessar perfil no Github</a>
+          </li>
+          <li className="devItem">
+            <header>
+              <img src="https://avatars0.githubusercontent.com/u/45516090?s=460&v=4" alt="Guilherme Ulbriki"/>
+              <div className="user_info">
+                <strong>Guilherme Ulbriki</strong>
+                <span>ReactJS, Reacte Native, NodeJS</span>
+              </div>
+            </header>
+            <p>IFFar Tecnico em Informatica - Campus Frederico Westphalen</p>
+            <a href="https://github.com/guilhermeulbriki">Acessar perfil no Github</a>
+          </li>
+        </ul>
+      </main>
     </div>
   );
-}
+};
 
 export default App;
