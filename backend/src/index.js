8865154fad88ceef07475f571c36e743c045cd62
guilhermeulbriki <guiulbriki@gmail.com>
2020-01-15 21:56:17 -0300

    deu erro

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,7 +5,7 @@ const routes = require('./routes');
 
 const app = express();
 
-mongoose.connect('mongodb+srv://admin:admin@cluster0-m3k1k.gcp.mongodb.net/test?retryWrites=true&w=majority', {
+mongoose.connect('mongodb+srv://admin:admin@cluster0-pmqdy.gcp.mongodb.net/test?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
